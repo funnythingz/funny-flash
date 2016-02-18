6cc62d943e3b8a2909e611a3441c67512fc4c364
funnythingz <funnythingz69+github@gmail.com>
2016-02-19 00:56:27 +0900

    added sketch

---
+++ app/src/main/kotlin/com/funnythingz/funnyrainbow/Sketch.kt
@@ -0,0 +1,68 @@
+package com.funnythingz.funnyrainbow
+
+import processing.core.PApplet
+
+class Sketch : PApplet() {
+
+    private var wpx: Float = 0f
+    private var hpx: Float = 0f
+
+    private enum class BackgroundColor {
+        BLACK,
+        RANDOM
+    }
+
+    private var backgroundColor: BackgroundColor? = null
+
+    private fun switchBackgroundColor() {
+        when (this.backgroundColor) {
+            Sketch.BackgroundColor.BLACK -> {
+                this.backgroundColor = BackgroundColor.RANDOM
+                background(random(256f), random(256f), random(256f))
+            }
+            Sketch.BackgroundColor.RANDOM -> {
+                this.backgroundColor = BackgroundColor.BLACK
+                background(0)
+            }
+        }
+    }
+
+    private fun selectBackgroundColor(backgroundColor: BackgroundColor) {
+        this.backgroundColor = backgroundColor
+        when (this.backgroundColor) {
+            Sketch.BackgroundColor.BLACK -> background(0)
+            Sketch.BackgroundColor.RANDOM -> background(random(256f), random(256f), random(256f))
+        }
+    }
+
+    override fun settings() {
+        fullScreen()
+    }
+
+    override fun setup() {
+        selectBackgroundColor(BackgroundColor.BLACK)
+        wpx = displayWidth / 10f
+        hpx = displayHeight / 16f
+    }
+
+    override fun draw() {
+        if (mousePressed) {
+            switchBackgroundColor()
+        }
+
+        var x: Float = 0f
+        while (x < width) {
+            var y: Float = 0f
+            while (y < height) {
+                noFill()
+                stroke(random(255f), random(255f), random(255f))
+                rect(x, y, wpx, hpx)
+                y += hpx
+            }
+            x += wpx
+        }
+        noFill()
+        stroke(random(255f), random(255f), random(255f))
+        rect(wpx * 9 - 1, hpx * 15 - 1, wpx, hpx)
+    }
+}
